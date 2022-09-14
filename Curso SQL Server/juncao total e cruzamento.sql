9e0e37a28bd150b475b336e1451386f403ce56ed
unknown <izinis.caroline@gmail.com>
2022-09-13 22:14:08 -0300

    Exercício de Junção Total e por Cruzamento Cartesiano

---
+++ Curso SQL Server/juncao total e cruzamento.sql
@@ -0,0 +1,21 @@
+USE SisDep;
+
+SELECT
+	F.NomeFuncionario, F.Admissao, D.NomeDependente, D.NascimentoDependente
+FROM Funcionario AS F FULL JOIN Dependente AS D
+ON F.idMatricula = D.idMatricula
+ORDER BY F.NomeFuncionario;
+
+
+SELECT * FROM Depto;
+
+
+SELECT * FROM Projeto;
+
+
+SELECT
+	D.NomeDepartamento, 
+	P.NomeProjeto,
+	P.DataInicio,
+	P.DataInicio
+FROM DEPTO AS D CROSS JOIN Projeto P;
